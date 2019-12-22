77c89bbae9cab804a7d322168374bcf0ffc08b93
tbenst <tbenst@gmail.com>
2019-12-22 14:09:42 -0800

    Update release.nix

--- release.nix
+++ release.nix
@@ -1,7 +1,8 @@
 { ... }:
 let
+  nixpkgsSHA = "07be0186f298e5b16897a168eae6ab01a5540fc4";
   pkgs = import (fetchTarball
-    https://github.com/nixos/nixpkgs/archive/master.tar.gz);
+    https://github.com/tbenst/nixpkgs/archive/${nixpkgsSHA}.tar.gz)
 in {
   hello = pkgs.hello;
 }
